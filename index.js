aabea0fa30cdf12b6158a5fc51261fbe205db5bb
1000ch <shogo.sensui@gmail.com>
2015-11-10 02:48:01 +0900

    modular

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const parseXML = require('xml-parser');
+
+const createIndent = depth => Array(depth * 2 + 1).join(' ');
+
+const createAttributes = attributes => {
+  let html = '';
+  Object.keys(attributes).forEach(key => {
+    html += ` ${key}="${attributes[key]}"`;
+  });
+  return html;
+};
+
+const createOpenTag = object => `<${object.name}${createAttributes(object.attributes)}>`;
+
+const createCloseTag = object => `</${object.name}>`;
+
+const createIsolateTag = object => {
+  if (object.content) {
+    return `<${object.name}${createAttributes(object.attributes)}>${object.content}</${object.name}>`;
+  } else {
+    return `<${object.name}${createAttributes(object.attributes)} />`;
+  }
+};
+
+const createXML = (object, depth) => {
+  let xml = '';
+  if (object.children.length) {
+    xml += `${createIndent(depth)}${createOpenTag(object)}\n`;
+    if (object.content) {
+      xml += `${createIndent(depth + 1)}${object.content}\n`;
+    }
+    object.children.forEach(child => {
+      xml += createXML(child, depth + 1);
+    });
+    xml += `${createIndent(depth)}${createCloseTag(object)}\n`;
+  } else {
+    xml += `${createIndent(depth)}${createIsolateTag(object)}\n`;
+  }
+  return xml;
+};
+
+const formatXML = (string, indent) => {
+  let xml = '';
+  try {
+    let ast = parseXML(string);
+    if (ast.declaration) {
+      xml += `<?xml${createAttributes(ast.declaration.attributes)}?>\n`;
+    }
+    xml += createXML(ast.root, 0);
+  } catch (e) {
+    return string;
+  }
+  return xml;
+};
+
+module.exports = formatXML;
