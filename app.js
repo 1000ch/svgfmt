dccc1341dd4787eb73cf96152a9f33380ee38a17
1000ch <shogo.sensui@gmail.com>
2015-11-10 02:48:58 +0900

    rebuild

--- app.js
+++ app.js
@@ -1,82 +1,6 @@
 'use strict'
 
-const parseXML = require('xml-parser');
-
-const createIndent = depth => Array(depth * 2 + 1).join(' ');
-
-const createAttributes = attributes => {
-
-  let keys = Object.keys(attributes);
-
-  if (keys.length) {
-    return keys.map(key => ` ${key}="${attributes[key]}"`);
-  } else {
-    return '';
-  }
-};
-
-const createOpenTag = object => `<${object.name}${createAttributes(object.attributes)}>`;
-
-const createCloseTag = object => `</${object.name}>`;
-
-const createIsolateTag = object => {
-
-  let name = object.name;
-  let attributes = createAttributes(object.attributes);
-  let content = object.content;
-
-  if (content) {
-    return `<${name}${attributes}>${content}</${name}>`;
-  } else {
-    return `<${name}${attributes} />`;
-  }
-};
-
-const createXML = (object, depth) => {
-
-  let xml = '';
-  let indent = createIndent(depth);
-  let children = object.children;
-  let content = object.content;
-
-  if (children.length) {
-    xml += `${indent}${createOpenTag(object)}\n`;
-    if (content) {
-      xml += `${createIndent(depth + 1)}${content}\n`;
-    }
-    children.forEach(child => {
-      xml += createXML(child, depth + 1);
-    });
-    xml += `${indent}${createCloseTag(object)}\n`;
-  } else {
-    xml += `${indent}${createIsolateTag(object)}\n`;
-  }
-  return xml;
-};
-
-const formatXML = (string, indent) => {
-
-  let xml = '';
-
-  try {
-    let ast = parseXML(string);
-    let declaration = ast.declaration;
-    let root = ast.root;
-
-    if (declaration) {
-      let attributes = createAttributes(declaration);
-      xml += `<xml${attributes}>`;
-    }
-
-    if (root) {
-      xml += createXML(root, 0);
-    }
-  } catch (e) {
-    return string;
-  }
-
-  return xml;
-};
+const formatXML = require('./');
 
 window.onload = e => {
   const formatInput  = document.querySelector('#format-input');
@@ -86,6 +10,6 @@ window.onload = e => {
 
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('service-worker.js', {
-    scope: '/xml-format/'
+    scope: '/xmlfmt/'
   }).catch(error => console.error(error));
 }
