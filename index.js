deaf9a4d7ef65f9e7f9a5b88a61d64fdefb4dc3e
1000ch <shogo.sensui@gmail.com>
2016-02-17 19:23:37 +0900

    use xml-stringify

--- index.js
+++ index.js
@@ -1,60 +1,16 @@
 'use strict';
 
-const parseXML = require('xml-parser');
+const parse = require('xml-parser');
+const stringify = require('xml-stringify');
 
-const createIndent = depth => Array(depth * 2 + 1).join(' ');
-
-const createAttributes = attributes => {
-  let html = '';
-  Object.keys(attributes).forEach(key => {
-    html += ` ${key}="${attributes[key]}"`;
-  });
-  return html;
-};
-
-const createOpenTag = object => `<${object.name}${createAttributes(object.attributes)}>`;
-
-const createCloseTag = object => `</${object.name}>`;
-
-const createIsolateTag = object => {
-  let xml = '';
-  if (object.content) {
-    xml += `<${object.name}${createAttributes(object.attributes)}>${object.content}</${object.name}>`;
-  } else {
-    xml += `<${object.name}${createAttributes(object.attributes)} />`;
-  }
-  return xml;
-};
-
-const createXML = (object, depth) => {
-  let xml = '';
-  if (object.children.length) {
-    xml += `${createIndent(depth)}${createOpenTag(object)}\n`;
-    if (object.content) {
-      xml += `${createIndent(depth + 1)}${object.content}\n`;
-    }
-    object.children.forEach(child => {
-      xml += createXML(child, depth + 1);
-    });
-    xml += `${createIndent(depth)}${createCloseTag(object)}\n`;
-  } else {
-    xml += `${createIndent(depth)}${createIsolateTag(object)}\n`;
-  }
-  return xml;
-};
-
-const formatXML = string => {
-  let xml = '';
+function svgfmt(string) {
   try {
-    let ast = parseXML(string);
-    if (ast.declaration) {
-      xml += `<?xml${createAttributes(ast.declaration.attributes)}?>\n`;
-    }
-    xml += createXML(ast.root, 0);
+    let ast = parse(string);
+    return stringify(ast);
   } catch (e) {
-    return string;
+    throw e;
   }
-  return xml;
-};
+  return string;
+}
 
-module.exports = formatXML;
+module.exports = svgfmt;
